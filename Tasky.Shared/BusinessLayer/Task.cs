0942d3e22dcc37d6391d7ef113b89f1042f3ee9b
Ing <Ing@DESKTOP-4VF74C1>
2016-08-08 15:12:12 +0200

    Add project files.

---
+++ Tasky.Shared/BusinessLayer/Task.cs
@@ -0,0 +1,25 @@
+using System;
+using Tasky.BL.Contracts;
+using SQLite;
+
+namespace Tasky.BL
+{
+	/// <summary>
+	/// Represents a Task.
+	/// </summary>
+	public class Task : IBusinessEntity
+	{
+		public Task ()
+		{
+		}
+
+		[PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+		public string Name { get; set; }
+		public string Notes { get; set; }
+        public string[] SubTasks { get; set; }
+		// new property
+		public bool Done { get; set; }
+	}
+}
+
